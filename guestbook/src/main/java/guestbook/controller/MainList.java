fe44597850cecd7ec9847bfcb26453f3a899f2b9
gayeong <sgyoung96@naver.com>
2023-03-29 14:34:23 +0900

    1. servlet test 2. server.xml > path 수정 (/로 수정)

---
+++ guestbook/src/main/java/guestbook/controller/MainList.java
@@ -0,0 +1,47 @@
+package guestbook.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MainList
+ */
+@WebServlet("/guestbook/list")
+public class MainList extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MainList() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		// response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		/* 화면 이동시 사용하는 코드 */
+		RequestDispatcher dis = request.getRequestDispatcher("/guestbook/list.jsp");
+		dis.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
--- guestbook/src/main/webapp/guestbook/list.jsp
+++ guestbook/src/main/webapp/guestbook/list.jsp
@@ -4,9 +4,9 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Insert title here</title>
+<title>방명록</title>
 </head>
 <body>
-
+	Test!!!
 </body>
 </html>
